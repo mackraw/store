b5089f6c331f07f95fd3f7d0076ca3a41f670928
mackraw <maciekrawczyk@gmail.com>
2021-11-11 01:01:26 -0600

    first mobile version of the nav

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,38 @@
 import Link from 'next/link';
+import { RiShoppingCartLine, RiSearchLine, RiMenu2Line, RiCloseFill } from 'react-icons/ri';
 
 export default function Header() {
   return (
-    <header className="site-header">
-      <div className="burger-btn-wrapper">
-        <div className="burger-btn"></div>
-      </div>
-      <Link href='/'><a className='logo'>homeGoods</a></Link>
-      <nav className="nav">
+    <header className='site-header container-inner'>
+      <span className='burger-btn'>
+        <RiMenu2Line />
+        <RiCloseFill className='is-hidden' />
+      </span>
+      <Link href='/'>
+        <a className='logo'>homeGoods</a>
+      </Link>
+      <nav className='nav is-hidden'>
         <ul>
           <li className='nav-item'>
-            <Link href='/shop'><a className='nav-link'>Shop </a></Link>
+            <Link href='/shop'>
+              <a className='nav-link'>Shop </a>
+            </Link>
           </li>
           <li className='nav-item'>
-            <Link href='/login'><a className='nav-link'>Log In</a></Link>
+            <Link href='/login'>
+              <a className='nav-link'>Log In</a>
+            </Link>
           </li>
         </ul>
       </nav>
-      <button type='button' className='cart'>Cart</button>
-      <div className='nav-search'>Search</div>
+      <div>
+        <span className='cart'>
+          <RiShoppingCartLine />
+        </span>
+        <span className='nav-search'>
+          <RiSearchLine />
+        </span>
+      </div>
     </header>
   );
 }
