110304e052f0b22b7c8e90c5b46c0c1b31a5152b
mackraw <maciekrawczyk@gmail.com>
2021-11-12 00:18:12 -0600

    add mobile styles to nav and state

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,30 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import { RiShoppingCartLine, RiSearchLine, RiMenu2Line, RiCloseFill } from 'react-icons/ri';
 
 export default function Header() {
+  const [isMobileNavHidden, setIsMobileNavHidden] = useState(true);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavHidden(!isMobileNavHidden);
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('is-showing');
+  };
+
   return (
     <header className='site-header container-inner'>
       <span className='burger-btn'>
-        <RiMenu2Line />
-        <RiCloseFill className='is-hidden' />
+        <RiMenu2Line className={isMobileNavHidden ? null : 'is-hidden'} onClick={toggleMobileNav} />
+        <RiCloseFill className={isMobileNavHidden ? 'is-hidden' : null} onClick={toggleMobileNav} />
       </span>
       <Link href='/'>
         <a className='logo'>homeGoods</a>
       </Link>
-      <nav className='nav is-hidden'>
+      <nav className='nav'>
         <ul>
           <li className='nav-item'>
             <Link href='/shop'>
-              <a className='nav-link'>Shop </a>
+              <a className='nav-link'>Shop</a>
             </Link>
           </li>
           <li className='nav-item'>
