6fd840501f0a73cb4639c0a5150ddb4c049a3cbe
Maciej Krawczyk <maciekrawczyk@gmail.com>
2022-01-14 11:16:58 -0600

    added apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
-import Page from '../components/Page';
-import './../components/styles/main.sass';
+import Page from "../components/Page";
+import "./../components/styles/main.sass";
+import { ApolloProvider } from "@apollo/client";
+import withData from "../lib/withData";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
