4d005ab2370ce4b1bad2f19ebb3793274c0c27d2
Mac Krawczyk <maciekrawczyk@gmail.com>
2022-05-21 10:56:56 -0500

    added classes for refactored styles

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,12 +1,31 @@
+import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import CartBtn from './CartBtn';
 
 export default function ProductCard({ product }) {
   return (
     <div className="product">
-      <img className="product-image" src={product?.photo?.image?.publicUrlTransformed} alt={product.name}/>
-      <div className="product-title">{product.name}</div>
+      <Link href={`/product/${product.id}`}>
+        <a className="image-link">
+          <img
+            className="product-image"
+            src={product?.photo?.image?.publicUrlTransformed}
+            alt={product.name}
+          />
+        </a>
+      </Link>
+      <div className="product-category">{product.category}</div>
+      <div className="product-title">
+        <Link href={`/product/${product.id}`}>
+          <a className="title-link">{product.name}</a>
+        </Link>
+      </div>
       <div className="product-description">{product.description}</div>
-      <div className="product-price">{formatMoney(product.price)}</div>
+      <div className="bottom-row">
+        <div className="product-price">{formatMoney(product.price)}</div>
+        {/* <button type="button" className='product-btn'>+ Add to Cart</button> */}
+        <CartBtn className="cardBtn">+ Add to Cart</CartBtn>
+      </div>
     </div>
   );
 }
