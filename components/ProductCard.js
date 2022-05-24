f4052ebedd769eb536d5bb948f85aca2cf43637a
Mac Krawczyk <maciekrawczyk@gmail.com>
2022-05-23 21:55:50 -0500

    refactored card styles

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,26 +4,29 @@ import CartBtn from './CartBtn';
 
 export default function ProductCard({ product }) {
   return (
-    <div className="product">
-      <Link href={`/product/${product.id}`}>
-        <a className="image-link">
-          <img
-            className="product-image"
-            src={product?.photo?.image?.publicUrlTransformed}
-            alt={product.name}
-          />
-        </a>
-      </Link>
-      <div className="product-category">{product.category}</div>
-      <div className="product-title">
+    <div className="product-card">
+      <div className="product-image-wrapper">
         <Link href={`/product/${product.id}`}>
-          <a className="title-link">{product.name}</a>
+          <a>
+            <img
+              className="product-image"
+              src={product?.photo?.image?.publicUrlTransformed}
+              alt={product.name}
+            />
+          </a>
         </Link>
       </div>
-      <div className="product-description">{product.description}</div>
+      <div className="card-content">
+        <div className="product-category">{product.category}</div>
+        <div className="product-title">
+          <Link href={`/product/${product.id}`}>
+            <a className="title-link">{product.name}</a>
+          </Link>
+        </div>
+        <div className="product-description">{product.description}</div>
+      </div>
       <div className="bottom-row">
         <div className="product-price">{formatMoney(product.price)}</div>
-        {/* <button type="button" className='product-btn'>+ Add to Cart</button> */}
         <CartBtn className="cardBtn">+ Add to Cart</CartBtn>
       </div>
     </div>
