bca3218b2969bd11d54a0f3199f3ddb1d4fc3a2b
Mac Krawczyk <maciekrawczyk@gmail.com>
2022-05-27 22:56:15 -0500

    refactored to work well with footer

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,13 @@
 import propTypes from 'prop-types';
 import Header from './Header';
+import Footer from './Footer';
 
 export default function Page({ children }) {
   return (
     <>
       <Header />
-      {children}
+      <main className="main">{children}</main>
+      <Footer />
     </>
   );
 }
