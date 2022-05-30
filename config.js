93e5ece1e1070fbe7794cbf141ce738f1b16c555
Mac Krawczyk <maciekrawczyk@gmail.com>
2022-05-30 13:54:10 -0500

    updated production config

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://home-goods-store.herokuapp.com/`;
+export const prodEndpoint = `https://home-goods-store.herokuapp.com/api/graphql`;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-import Page from '../components/Page';
+import Categories from '../components/Categories';
+import Hero from '../components/Hero';
+import OrderToday from '../components/OrderToday';
 
 export default function IndexPage() {
-  return <p>Howdy from Index</p>;
+  return (
+    <>
+      <Hero
+        title="Come inside!"
+        subtitle="Check out available products and functionality."
+        heroStyles="index-hero"
+        textStyles="index-hero-text"
+      />
+      {/* create account, add items to cart, search for items */}
+      <Categories />;
+      <OrderToday />
+    </>
+  );
 }
